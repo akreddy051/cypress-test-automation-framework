c79b6bda475b97ee3eabc0a5c9647f044927a811
Akshay Singadiwar <sakshayreddy66@gmail.com>
2024-03-24 00:24:49 +0530

    added cucumber dependencies and the feature files to the framework

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false,
   },
 });
